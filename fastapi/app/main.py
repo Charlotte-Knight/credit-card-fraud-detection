6eb1e368bcb4243b47a3ce4320b44afc752acbb2
Charlotte Knight <charlotte.knight@cern.ch>
2025-08-25 17:54:37 +0100

    Initial commit

---
+++ fastapi/app/main.py
@@ -0,0 +1,250 @@
+from datetime import datetime
+from typing import Annotated
+
+from fastapi import Depends, FastAPI, HTTPException, Query
+from sqlmodel import Field, Relationship, Session, SQLModel, create_engine, select, delete
+
+import random
+
+from prometheus_fastapi_instrumentator import Instrumentator
+
+class CustomerBase(SQLModel):
+  LocationX: float = Field(ge=0, le=100)
+  LocationY: float = Field(ge=0, le=100)
+  AmountMean: float
+  AmountStd: float
+  TxRate: float
+  
+class Customer(CustomerBase, table=True):
+  CustomerID: int | None = Field(default=None, primary_key=True)
+
+class CustomerPublic(CustomerBase):
+  CustomerID: int
+
+class TerminalBase(SQLModel):
+  LocationX: float = Field(ge=0, le=100)
+  LocationY: float = Field(ge=0, le=100)
+
+class Terminal(TerminalBase, table=True):
+  TerminalID: int | None = Field(default=None, primary_key=True)
+
+class TerminalPublic(TerminalBase):
+  TerminalID: int
+
+class TransactionBase(SQLModel):
+  Time: datetime
+  CustomerID: int = Field(foreign_key="customer.CustomerID")
+  TerminalID: int = Field(foreign_key="terminal.TerminalID")
+  Amount: float = Field(ge=0)
+  Fraud: bool = False
+  FraudScenario: int = 0
+
+class Transaction(TransactionBase, table=True):
+  TransactionID: int | None = Field(default=None, primary_key=True)
+
+class TransactionPublic(TransactionBase):
+  TransactionID: int
+
+class FraudBase(SQLModel):
+  CustomerID: int | None = Field(foreign_key="customer.CustomerID", default=None)
+  TerminalID: int | None = Field(foreign_key="terminal.TerminalID", default=None)
+  AmountMean: float
+  AmountStd: float
+  TxRate: float
+  StartTime: datetime
+  EndTime: datetime
+
+class Fraud(FraudBase, table=True):
+  FraudID: int | None = Field(default=None, primary_key=True)
+
+class FraudPublic(FraudBase):
+  FraudID: int
+
+engine = create_engine("postgresql://postgres:password@postgres/postgres")
+
+def create_db_and_tables():
+  SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+  with Session(engine) as session:
+    yield session
+
+SessionDep = Annotated[Session, Depends(get_session)]
+app = FastAPI()
+
+Instrumentator().instrument(app).expose(app)
+
+@app.on_event("startup")
+def on_startup():
+  create_db_and_tables()
+
+"""----------------------------------------------------------------------------------------------"""
+
+@app.post("/customers/", response_model=CustomerPublic)
+def create_customer(customer: CustomerBase, session: SessionDep):
+  db_customer = Customer.model_validate(customer)
+  session.add(db_customer)
+  session.commit()
+  session.refresh(db_customer)
+  return db_customer
+
+@app.post("/customers/batch/", response_model=list[CustomerPublic])
+def create_customers_batch(customers: list[CustomerBase], session: SessionDep):
+  db_customers = [Customer.model_validate(customer) for customer in customers]
+  session.add_all(db_customers)
+  session.commit()
+  for customer in db_customers:
+    session.refresh(customer)
+  return db_customers
+
+@app.get("/customers/", response_model=list[CustomerPublic])
+def read_customers(session: SessionDep):
+  customers = session.exec(select(Customer)).all()
+  return customers
+
+@app.get("/customers/{customer_id}", response_model=CustomerPublic)
+def read_customer(customer_id: int, session: SessionDep):
+  customer = session.get(Customer, customer_id)
+  if not customer:
+    raise HTTPException(status_code=404, detail="Customer not found")
+  return customer
+
+"""----------------------------------------------------------------------------------------------"""
+
+@app.post("/terminals/", response_model=TerminalPublic)
+def create_terminal(terminal: TerminalBase, session: SessionDep):
+  db_terminal = Terminal.model_validate(terminal)
+  session.add(db_terminal)
+  session.commit()
+  session.refresh(db_terminal)
+  return db_terminal
+
+@app.post("/terminals/batch/", response_model=list[TerminalPublic])
+def create_terminals_batch(terminals: list[TerminalBase], session: SessionDep):
+  db_terminals = [Terminal.model_validate(terminal) for terminal in terminals]
+  session.add_all(db_terminals)
+  session.commit()
+  for terminal in db_terminals:
+    session.refresh(terminal)
+  return db_terminals
+
+@app.get("/terminals/", response_model=list[TerminalPublic])
+def read_terminals(session: SessionDep):
+  terminals = session.exec(select(Terminal)).all()
+  return terminals
+
+@app.get("/terminals/{terminal_id}", response_model=TerminalPublic)
+def read_terminal(terminal_id: int, session: SessionDep):
+  terminal = session.get(Terminal, terminal_id)
+  if not terminal:
+    raise HTTPException(status_code=404, detail="Terminal not found")
+  return terminal
+
+"""----------------------------------------------------------------------------------------------"""
+
+@app.post("/frauds/", response_model=FraudPublic)
+def create_fraud(fraud: FraudBase, session: SessionDep):
+  db_fraud = Fraud.model_validate(fraud)
+  session.add(db_fraud)
+  session.commit()
+  session.refresh(db_fraud)
+  return db_fraud
+
+@app.post("/frauds/batch/", response_model=list[FraudPublic])
+def create_frauds_batch(frauds: list[FraudBase], session: SessionDep):
+  db_frauds = [Fraud.model_validate(fraud) for fraud in frauds]
+  session.add_all(db_frauds)
+  session.commit()
+  for fraud in db_frauds:
+    session.refresh(fraud)
+  return db_frauds
+
+@app.get("/frauds/", response_model=list[FraudPublic])
+def read_frauds(session: SessionDep):
+  frauds = session.exec(select(Fraud)).all()
+  return frauds
+
+@app.get("/frauds/{fraud_id}", response_model=FraudPublic)
+def read_fraud(fraud_id: int, session: SessionDep):
+  fraud = session.get(Fraud, fraud_id)
+  if not fraud:
+    raise HTTPException(status_code=404, detail="Fraud not found")
+  return fraud
+
+"""----------------------------------------------------------------------------------------------"""
+
+@app.post("/transactions/", response_model=TransactionPublic)
+def create_transaction(transaction: TransactionBase, session: SessionDep):
+  db_transaction = Transaction.model_validate(transaction)
+  session.add(db_transaction)
+  session.commit()
+  session.refresh(db_transaction)
+  return db_transaction
+
+@app.post("/transactions/batch/", response_model=list[TransactionPublic])
+def create_transactions_batch(transactions: list[TransactionBase], session: SessionDep):
+  db_transactions = [Transaction.model_validate(transaction) for transaction in transactions]
+  session.add_all(db_transactions)
+  session.commit()
+  for transaction in db_transactions:
+    session.refresh(transaction)
+  return db_transactions
+
+@app.get("/transactions/", response_model=list[TransactionPublic])
+def read_transactions(
+  session: SessionDep,
+  start_date: datetime | None = None,
+  end_date: datetime | None = None,
+  customer_id: int | None = None,
+  fraud: bool | None = None
+):
+  query = select(Transaction)
+  if start_date:
+    query = query.where(Transaction.TX_DATETIME >= start_date)
+  if end_date:
+    query = query.where(Transaction.TX_DATETIME <= end_date)
+  if customer_id:
+    query = query.where(Transaction.CustomerID == customer_id)
+  if fraud is not None:
+    query = query.where(Transaction.TX_FRAUD == fraud)
+
+  transactions = session.exec(query).all()
+  return transactions
+
+@app.get("/transactions/locations/")
+def read_transaction_locations(session: SessionDep) -> list[tuple[Transaction, Terminal]]:
+  statement = select(Transaction, Terminal).where(Transaction.TerminalID == Terminal.TerminalID)
+  results = session.exec(statement).all()
+  return results
+
+@app.get("/transactions/{transaction_id}", response_model=TransactionPublic)
+def read_transaction(transaction_id: int, session: SessionDep):
+  transaction = session.get(Transaction, transaction_id)
+  if not transaction:
+    raise HTTPException(status_code=404, detail="Transaction not found")
+  return transaction
+
+"""----------------------------------------------------------------------------------------------"""
+
+@app.delete("/delete_all_data/")
+def delete_all_data(session: SessionDep):
+  session.exec(delete(Transaction))
+  session.exec(delete(Fraud))
+  session.exec(delete(Terminal))
+  session.exec(delete(Customer))
+  session.commit()
+  
+"""----------------------------------------------------------------------------------------------"""
+
+@app.get("/randomint")
+async def get_random_int():
+  return random.randint(1, 100)
+
+@app.get("/test")
+async def test_endpoint():
+  return None
+
+@app.get("/health")
+def health_check():
+    return {"status": "healthy"}
---
+++ synthetic_data/app/main.py
@@ -0,0 +1,219 @@
+import numpy as np
+import pandas as pd
+import requests
+import argparse
+from tqdm import tqdm
+from apscheduler.schedulers.background import BlockingScheduler
+from dataclasses import dataclass
+
+API_URL = "http://fastapi:80/"
+
+@dataclass
+class TimeInfo:
+  start_date: pd.Timestamp
+  n_periods: int
+  period_length: int
+
+def generate_locations(n: int, r : tuple[float, float] = (0,100)) -> pd.DataFrame:
+  locations = {
+    "LocationX": np.random.uniform(r[0], r[1], n),
+    "LocationY": np.random.uniform(r[0], r[1], n)
+  }
+  return pd.DataFrame(locations).round(4)
+
+def generate_transaction_characters(n: int,
+                                    amount_mean: tuple[float, float] = (0.1, 10),
+                                    tx_rate :    tuple[float, float] = (0.1, 10)) -> pd.DataFrame:
+  characters = {
+    "AmountMean": np.random.uniform(amount_mean[0], amount_mean[1], n),
+    "TxRate":     np.random.uniform(    tx_rate[0],     tx_rate[1], n)
+  }
+  characters["AmountStd"] = characters["AmountMean"] / 2
+  return pd.DataFrame(characters).round(4)
+
+def generate_times(rate: float, time_info: TimeInfo) -> pd.Series:
+  n = int(time_info.n_periods * rate) + 1
+  time_deltas = np.random.exponential(1/rate, n).cumsum() * time_info.period_length
+  time_deltas = pd.to_timedelta(time_deltas, unit='m')
+  return time_info.start_date + time_deltas
+
+def generate_time_windows(rate: float, time_info: TimeInfo,
+                          duration: tuple[float, float] = (0.1, 10)) -> pd.DataFrame:
+  start_times = generate_times(rate, time_info)
+  durations = np.random.uniform(duration[0], duration[1], len(start_times)) * time_info.period_length
+  durations = pd.to_timedelta(durations, unit='m')
+  time_windows = {
+    "StartTime": start_times,
+    "EndTime": start_times + durations
+  }
+
+  return pd.DataFrame(time_windows)
+
+def generate_customers(n_customers: int):
+  locations = generate_locations(n_customers)
+  tx_characters = generate_transaction_characters(n_customers)
+  customers = locations.join(tx_characters).round(4)
+
+  return customers
+
+def generate_terminals(n_terminals: int):
+  locations = generate_locations(n_terminals).round(4)
+  return locations
+
+def generate_transactions(character: pd.Series, time_info: TimeInfo) -> pd.DataFrame:
+  times = generate_times(character.TxRate, time_info)
+  Amount = np.random.normal(character.AmountMean, character.AmountStd, len(times))
+  Amount = np.clip(Amount, 0.01, None).round(2)
+
+  transactions = {
+    "Time": times,
+    "Amount": Amount
+  }
+
+  return pd.DataFrame(transactions)
+
+def generate_frauds(players: pd.DataFrame, time_info: TimeInfo,
+                    fraud_rate_fraction: float = 0.01) -> pd.DataFrame:
+  fraud_rate = len(players) * fraud_rate_fraction
+
+  time_windows = generate_time_windows(fraud_rate, time_info)
+  characters = generate_transaction_characters(len(time_windows))
+
+  frauds = time_windows.join(characters)
+  frauds[players.index.name] = np.random.choice(players.index, len(frauds))
+
+  return frauds
+
+def get_distances(customer: pd.Series, terminals: pd.DataFrame) -> pd.Series:
+  displacements = terminals[["LocationX", "LocationY"]] - customer[["LocationX", "LocationY"]]
+  distances = np.sqrt((displacements**2).sum(axis=1))
+  return pd.Series(distances, index=terminals.index)
+
+def generate_genuine_transactions(customers: pd.DataFrame, terminals: pd.DataFrame,
+                                  time_info: TimeInfo) -> pd.DataFrame:
+  transactions = []
+
+  for CustomerID, customer in tqdm(customers.iterrows(), desc="Generating customer transactions",
+                                   total=customers.shape[0]):
+    customer_transactions = generate_transactions(customer, time_info)
+
+    distances = get_distances(customer, terminals)
+    p = np.exp(-distances / distances.std())
+    p /= p.sum()
+    customer_transactions["TerminalID"] = np.random.choice(terminals.index, len(customer_transactions), p=p)
+
+    customer_transactions["CustomerID"] = CustomerID
+    transactions.append(customer_transactions)
+
+  transactions = pd.concat(transactions, ignore_index=True)
+  transactions["Fraud"] = False
+
+  return transactions
+
+def generate_fraudulent_customer_transactions(frauds: pd.DataFrame, terminals: pd.DataFrame,
+                                              time_info: TimeInfo) -> pd.DataFrame:
+  fraudulent_transactions = []
+
+  for FraudID, fraud in tqdm(frauds.iterrows(), desc="Generating fraudulent customer transactions",
+                        total=frauds.shape[0]):
+    transactions = generate_transactions(fraud, time_info)
+    transactions = transactions[ (transactions.Time >= fraud.StartTime) & (transactions.Time <= fraud.EndTime) ]
+    
+    transactions["CustomerID"] = fraud["CustomerID"]
+    transactions["TerminalID"] = np.random.choice(terminals.index, len(transactions))
+
+    fraudulent_transactions.append(transactions)
+
+  fraudulent_transactions = pd.concat(fraudulent_transactions, ignore_index=True)
+  fraudulent_transactions["Fraud"] = True
+
+  return fraudulent_transactions
+
+def generate_fraudulent_terminal_transactions(frauds: pd.DataFrame, customers: pd.DataFrame,
+                                              time_info: TimeInfo) -> pd.DataFrame:
+  fraudulent_transactions = []
+
+  for FraudID, fraud in tqdm(frauds.iterrows(), desc="Generating fraudulent terminal transactions",
+                        total=frauds.shape[0]):
+    transactions = generate_transactions(fraud, time_info)
+    transactions = transactions[ (transactions.Time >= fraud.StartTime) & (transactions.Time <= fraud.EndTime) ]
+    
+    transactions["TerminalID"] = fraud["TerminalID"]
+    transactions["CustomerID"] = np.random.choice(customers.index, len(transactions))
+
+    fraudulent_transactions.append(transactions)
+
+  fraudulent_transactions = pd.concat(fraudulent_transactions, ignore_index=True)
+  fraudulent_transactions["Fraud"] = True
+
+  return fraudulent_transactions
+
+def delete_all_data():
+  response = requests.delete(API_URL + "delete_all_data/")
+  assert response.status_code == 200, response.text
+
+def send_table(df, path, batch_size=5000):
+  responses = []
+  for i in tqdm(range(0, df.shape[0], batch_size), desc=f"Sending to {path}"):
+    response = requests.post(API_URL + path,
+                             data=df.iloc[i:i+batch_size].to_json(orient='records'), timeout=60)
+    assert response.status_code == 200, response.text
+    responses.append(pd.DataFrame(response.json()))
+  return pd.concat(responses, ignore_index=True)
+
+def send_table_single(df, path):
+  response = requests.post(API_URL + path,
+                           data=df.to_json(), timeout=60)
+  assert response.status_code == 200, response.text
+
+def send_table_on_time(df, path):
+  df["Time"] += pd.Timestamp.now(tz='UTC') - df["Time"].min()
+  df["Time"] += pd.Timedelta(len(df)/10000, 's')
+
+  scheduler = BlockingScheduler()
+  for _, row in tqdm(df.iterrows(), desc="Scheduling transactions", total=df.shape[0]):
+    scheduler.add_job(send_table_single, 'date', run_date=row.Time, args=[row, path])
+  print(f"Transactions will begin at {df.iloc[0].Time}")
+  scheduler.start()
+
+def generate_send(generate_f: callable, generate_args: list, path: str, ID_col: str):
+  df = generate_f(*generate_args)
+  df = send_table(df, path)
+  df.set_index(ID_col, inplace=True)
+  return df
+
+def main(n_customers: int, n_terminals: int, n_periods: int, period_length: int,
+         clear_database: bool, seed: int):
+  np.random.seed(seed)
+  if clear_database:
+    delete_all_data()
+
+  start_date = pd.Timestamp.now(tz='UTC')
+  time_info = TimeInfo(start_date, n_periods, period_length)
+
+  customers = generate_send(generate_customers, [n_customers], "customers/batch/", "CustomerID")
+  terminals = generate_send(generate_terminals, [n_terminals], "terminals/batch/", "TerminalID")
+  customer_frauds = generate_send(generate_frauds, [customers, time_info], "frauds/batch/", "FraudID")
+  terminal_frauds = generate_send(generate_frauds, [terminals, time_info], "frauds/batch/", "FraudID")
+
+  genuine_transactions = generate_genuine_transactions(customers, terminals, time_info)
+  customer_fraud_transactions = generate_fraudulent_customer_transactions(customer_frauds, terminals, time_info)
+  terminal_fraud_transactions = generate_fraudulent_terminal_transactions(terminal_frauds, customers, time_info)
+
+  transactions = pd.concat([genuine_transactions, customer_fraud_transactions, terminal_fraud_transactions], ignore_index=True)
+
+  transactions.sort_values("Time", inplace=True)
+
+  send_table_on_time(transactions, "transactions/")
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser(description="Synthetic Data Generation")
+  parser.add_argument("--n-customers", "-c", type=int, default=5000, help="Number of new customers to generate")
+  parser.add_argument("--n-terminals", "-t", type=int, default=5000, help="Number of new terminals to generate")
+  parser.add_argument("--n-periods", "-n", type=int, default=2, help="Number of periods to generate transactions for")
+  parser.add_argument("--period-length", "-l", type=int, default=5, help="Length of a period in minutes")
+  parser.add_argument("--clear-database", action='store_true', help="Clear the database before generating new data")
+  parser.add_argument("--seed", "-s", type=int, default=42, help="Random seed for reproducibility")
+
+  args = parser.parse_args()
+  main(args.n_customers, args.n_terminals, args.n_periods, args.period_length, args.clear_database, args.seed)
