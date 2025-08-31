4ef099c8b4e7da9f8d9b338245c86624e9d22116
Charlotte Knight <charlotteroseknight@outlook.com>
2025-08-31 20:08:46 +0100

    more refactoring

--- up.sh
+++ up.sh
@@ -8,7 +8,7 @@ xdg-open http://localhost:8000/docs >/dev/null 2>&1 &
 xdg-open http://localhost:3000/d/ccfd/ccfd >/dev/null 2>&1 &
 
 tmux new-session -d -s monitoring watch -n 5 -t 'docker ps --format "table {{.Names}}\t{{.Status}}"'
-tmux split-window -h 'docker logs -f fastapi'
-tmux split-window -v 'docker logs -f synthetic_data'
-tmux set-hook client-resized 'resize-pane -t 0 -x 41'
+tmux split-window -h 'docker logs -f ccfd_api'
+tmux split-window -v 'docker logs -f transaction_generator'
+tmux set-hook client-resized 'resize-pane -t 0 -x 50'
 tmux attach -d
