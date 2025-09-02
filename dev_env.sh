94459c4fa3c4556e565a03a4421793f9f2e70285
Charlotte Knight <charlotteroseknight@outlook.com>
2025-09-02 19:25:22 +0100

    reduce env and container sizes

--- dev_env.sh
+++ dev_env.sh
@@ -10,11 +10,9 @@ else
   mv bin $env_dir
   export MAMBA_ROOT_PREFIX=$env_dir
   eval "$($env_dir/micromamba shell hook -s posix)"
-  echo "python=3.13" > $env_dir/requirements.txt
-  cat ccfd_api/requirements.txt transaction_generator/requirements.txt >> $env_dir/requirements.txt
-  echo ruff >> $env_dir/requirements.txt
-  echo pre-commit >> $env_dir/requirements.txt
-  micromamba env create -n $env_name -f $env_dir/requirements.txt
+  micromamba env create -n $env_name -f ccfd-api/env.yaml -y
+  micromamba install -n $env_name -f transaction-generator/env.yaml -y
+  micromamba install -n $env_name ruff pre-commit -y
   micromamba clean --all --yes
   micromamba activate $env_name
 fi
